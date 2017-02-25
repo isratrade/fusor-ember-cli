55e24f1be80118ed7762c6fbe1a2e85ef90ad6d2
Joseph Magen <jmagen@redhat.com>
2017-02-25 12:45:21 +0800

    fix some JS console errors caused by no settings or upstreamConsumer

--- app/serializers/organization.js
+++ app/serializers/organization.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.RESTSerializer.extend({
 
   normalize(modelClass, hash) {
-    if (hash.owner_details) {
+    if (hash.owner_details && hash.owner_details.upstreamConsumer) {
       hash.upstream_consumer_uuid = hash.owner_details.upstreamConsumer.uuid;
       hash.upstream_consumer_name = hash.owner_details.upstreamConsumer.name;
     }
