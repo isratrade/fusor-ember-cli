f66f2232b06cd32a2f17ebee9e0f12e128b88d81
Joseph Magen <jmagen@redhat.com>
2017-03-09 18:01:21 +0800

    squash this commit

--- app/mixins/needs-discovered-hosts-ajax.js
+++ app/mixins/needs-discovered-hosts-ajax.js
@@ -6,10 +6,12 @@ export default Ember.Mixin.create({
     const token = Ember.$('meta[name="csrf-token"]').attr('content');
     let hypervisorIds = hypervisorHosts.sortBy('id').getEach('id');
     let hypervisorNames = hypervisorHosts.sortBy('id').getEach('name');
+    let arrayIdsNames = hypervisorIds.concat(hypervisorNames);
+
     return request({
       url: window.fusorServer + '/fusor/api/v21/deployments/' + deployment.get('id'),
       type: 'PUT',
-      data: JSON.stringify({'deployment': { 'discovered_host_ids_names': (hypervisorIds + hypervisorNames) } }),
+      data: JSON.stringify({'deployment': { 'discovered_host_ids_names': arrayIdsNames } }),
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
