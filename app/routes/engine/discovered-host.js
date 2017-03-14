8e0a31138871d5023e797b02b49f69c5730f2c63
Joseph Magen <jmagen@redhat.com>
2017-03-14 17:46:56 +0800

    save engine to deployment_hosts

--- app/routes/engine/discovered-host.js
+++ app/routes/engine/discovered-host.js
@@ -1,13 +1,39 @@
 import Ember from 'ember';
 import DiscoveredHostRouteMixin from "../../mixins/discovered-host-route-mixin";
+import NeedsDiscoveredHostsAjax from '../../mixins/needs-discovered-hosts-ajax';
 
-export default Ember.Route.extend(DiscoveredHostRouteMixin, {
+export default Ember.Route.extend(DiscoveredHostRouteMixin, NeedsDiscoveredHostsAjax, {
   model() {
     return this.modelFor('deployment').get('discovered_host');
   },
 
+  setupController(controller, model) {
+    this._super(controller, model);
+
+    let deployment = this.modelFor('deployment');
+    this.set('saveOnTransition', deployment.get('isNotStarted'));
+  },
+
   deactivate() {
-    return this.send('saveDeployment', null);
+    this.send('saveDeployment', null);
+  },
+
+  actions: {
+    willTransition(transition) {
+      if (!this.get('saveOnTransition')) {
+        return true;
+      }
+
+      let deployment = this.modelFor('deployment');
+      let selectedRhevEngineHost = this.controllerFor('engine/discovered-host').get('selectedRhevEngineHost');
+      this.set('saveOnTransition', false);
+      transition.abort();
+      this.postDiscoveredEngineHost(deployment, selectedRhevEngineHost).catch(err => {
+        console.log(err);
+      }).finally(() => {
+        transition.retry();
+      });
+    }
   }
 
 });
